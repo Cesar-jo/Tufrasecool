6e66dde633dc5fa0eb9fd7392910f5c5353b9fa9
Cesar-jo <68834511+Cesar-jo@users.noreply.github.com>
2021-01-23 12:12:39 -0600

    Add files via upload

---
+++ Generador de fraces 2.0/scripts.js
@@ -0,0 +1,277 @@
+const quotes = [
+
+    {
+        frase: "Haz el amor y no la guerra.",
+        autor: " - John Lennon"
+    },
+    {
+
+        frase: "Cada dia savemos más y entendemos menos.",
+        autor: " - Albert Einstein"
+    },
+    {
+
+
+        frase: "La medida del amor es mas sin medida.",
+        autor: " - San Agustin"
+    },
+
+    {
+
+
+        frase: "Haz el amor y no la guerra.",
+        autor: " - John Lennon"
+    },
+
+    {
+
+        frase: "No hay nada que un hombre sea capaz de hacer cuando una mujer le mira.",
+        autor: " - Casanova"
+    },
+
+    {
+        frase: "El dinero no puede comprar la vida.",
+        autor: " - Bob Marley"
+    },
+
+    {
+        frase: "Yo no veo la destruccion que existe, si no la belleza que aun queda.",
+        autor: " - Anna Frank"
+    },
+
+    {
+        frase: "Son tres muchos y tres pocos los que destruyen a las personas:Mucho gastar y poco tener– Mucho hablar y poco saber– Mucho presumir y poco valer.",
+        autor: " - Proverbio español"
+    },
+
+    {
+        frase: "Hay dos maneras de vivir la vida: una como si nada fuese un milagro, la otra como si todo fuese un milagro.",
+        autor: " - Albert Einstein"
+    },
+
+    {
+        frase: "El secreto de la existencia humana no solo está en vivir, sino también en saber para qué se vive.",
+        autor: " - Fiódor Dostoioevski"
+    },
+
+    {
+        frase: "¿Amas la vida? Pues no desperdicies el tiempo, porque es la sustancia de la que está hecha.",
+        autor: " - Benjamín Franklin"
+    },
+
+    {
+        frase: "Vivir no es sólo existir y crear, es saber gozar y sufrir y no dormir sin soñar. Descansar es empezar a morir.",
+        autor: " - Gregorio Marañón"
+    },
+
+    {
+        frase: "Un hoy vale por dos mañanas.",
+        autor: " - Benjamín Franklin"
+    },
+
+    {
+        frase: "Aprendí que no se puede dar marcha atrás, que la esencia de la vida es ir hacia adelante.",
+        autor: " - Agatha Christie"
+    },
+
+    {
+        frase: "Tan pronto como uno es infeliz, uno se convierte en moralista.",
+        autor: " - Marcel Proust"
+    },
+
+    {
+        frase: "La vida no se mide por las veces que respiras, sino por los momentos que te dejan sin aliento.",
+        autor: " - De la película “Hitch”"
+    },
+
+    {
+        frase: "Cumplamos la tarea de vivir de tal modo que cuando muramos, incluso el de la funeraria lo sienta.",
+        autor: " -  Marc Twain."
+    },
+
+    {
+        frase: "Pregúntate si eres feliz, y dejarás de serlo.",
+        autor: " - John Stuart Mill"
+    },
+
+    {
+        frase: "Una persona que tiene 50 años y piensa como uno de 20, ha desperdiciado 30 años de vida.",
+        autor: " - Muhammad Alí"
+    },
+
+    {
+        frase: "Al final todo estara bien y si no lo esta es por que aun no es el final.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "La verdad es que todo tiene un precio, la pregunta es ¿Cuanto estas dispuesto a pagar?.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "La vida son dos dias, cuando naces y cuando mueres, asi que sonrrie, disfruta y aprende.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Me siento un tonto en confiar en alguien hoy en dia, en el planeta de la hipocresia.",
+        autor: " - Canserbero"
+    },
+
+    {
+        frase: "Hay dos futuros, el que depende de ti y el que otros deciden por ti, ¿Cual prefieres?.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Somos lo que queremos ser, uno vale lo que uno quiere.",
+        autor: " - Cesar_Jo"
+    },
+
+    {
+        frase: "La gente malagradecida solo se fija en lo que no pudiste hacer por ellos, pero nunca en lo que hiciste por ellos.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "La codicia cambia a las personas.",
+        autor: " - Anonimo"
+    },
+
+
+    {
+        frase: "Nuestro futuro depende de nosotros mismos.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Ser amable es lo que la gente tonta hace para mejorar sus opciones.",
+        autor: " - Rick & Morty"
+    },
+
+    {
+        frase: "Aveces tenemos mucho tiempo, pero a la vez nada que uno mismo se frustra.",
+        autor: " - Cesar_jo"
+    },
+
+    {
+        frase: "Y lo esperabas?, una vez alguien me dijo, pero es lo inesperado lo que te cambia la vida.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "El universo es tan grande como para preocuparse por algo tan pequeño.",
+        autor: " - Rick & Morty"
+    },
+
+    {
+        frase: "Nunca te vallas por las cosas faciles,siempre vete po lo complicado, recuerda que lo facil asi como viene se va y lo complicado si se llega a ir se aprecia mas por que cuesta mas y se valora mejor.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Ella es arcoiris y él es daltonico.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Para, hay cosas que no son entendibles, pero eso no quiere decir que no sean comprendibles, animo aveces las cosas no necesitan que las entiendan si no que las comprendan.",
+        autor: " - Cesar_jo"
+    },
+
+    {
+        frase: "Pierde tu tiempo con quien ganes vida,experiencias positivas y momentos.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Lo bueno de ser diferentes en una sociedad, es que nadie espera que seas como ellos.",
+        autor: " - Cesar_jo"
+    },
+
+    {
+        frase: "La noche tiene mil ojos y el dia solo uno; sin emvargo la luz brillante del mundo, muere con el sol moribundo.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Maldito el hombre que cree en otro hombre por la verdad en esta frase se esconde.",
+        autor: " - Canserbero"
+    },
+
+    {
+        frase: "Simple mente pense que tambien te gustaba, pero que crees, solo eran pensamientos y nada mas .",
+        autor: " - Cesar_jo"
+    },
+
+    {
+        frase: "Aveces necesitamos inyectarnos fantasía para no morir de realidad (y viseversa).",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Pocas veces pensamos en lo que tenemos, pero siempre en lo que nos falta.",
+        autor: " - epicuro"
+    },
+
+    {
+        frase: "Caer en los brazos de lo que deseamos y disfrutarlo es la unica forma de evitar el desasosiego que nos produce no tenerlo.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "La mejor manera de librarse de la tentación es caer en ella.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Tal vez estamos en el mundo para buscar el amor, encontrarlo y perderlo una y otra vez.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "La mayoria de la gente escucha con la intencion de responder y no con el deseo de comprender.",
+        autor: " - Anonimo"
+    },
+
+
+    {
+        frase: "Ellos han prometido que los sueños se cumplen, pero olvidaron mencionar que las pesadillas son sueños tambien.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Observa sus defectos y encontraras sus virtudes.",
+        autor: " - Anonimo"
+    },
+
+    {
+        frase: "Ami la soledad se me sento alado y cuando no está ¡¡La extraño!!.",
+        autor: " - Anonimo"
+    },
+
+    {
+
+        frase: "A la femine ni todo el amor, ni todo el dinero.",
+        autor: " - Anonimos"
+
+    }
+
+];
+
+
+/*Creamos una constante (Variable) llamada btn que va hacer nuestro boton del generador de frases y horita le pnemos una funcion*/
+const btn = document.getElementById("generate-btn");
+
+/*Le creamos un evento y su funcion que sera generar las fraces que hagregamos, le decimos que cuando le demos clic al btn se ejecute una funcion*/
+btn.addEventListener("click", function() {
+    /* con esto le decimos que nos genere con el random aleatoriamente una frace que agregamos*/
+    let random = Math.floor(Math.random() * quotes.length);
+    /* Con esto le decimos que cuando nos genere la frace nos la arroge en el contenedor que creamos en ese espacio */
+    document.getElementById("frase").textContent =
+        quotes[random].frase;
+    /* Esto lo que hara es seleccionar el autor de esa frase y ponerlo en el contenedor */
+    document.querySelector(".autor").textContent =
+        quotes[random].autor;
+});
